dbe04a1f9c380f265194c3bf88879b87ff463ef5
Dalio <lida@edison.tech>
2019-07-31 14:36:06 +0800

    test

--- examples/restaurantbot/__main__.py
+++ examples/restaurantbot/__main__.py
@@ -5,6 +5,6 @@ import rasa
 
 if __name__ == "__main__":
     os.chdir('/Users/lidayuan/Documents/edison/nlu/rasa/examples/restaurantbot')
-    rasa.train(domain='domain.yml', config='config.yml', training_files='./data')
-    # rasa.run(model="models", endpoints="endpoints.yml")
+    # rasa.train(domain='domain.yml', config='config.yml', training_files='./data')
+    rasa.run(model="models", endpoints="endpoints.yml")
 
