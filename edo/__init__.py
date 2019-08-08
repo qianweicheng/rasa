a574bb36ce454038e666bea0847c64a515173b7b
yuanlida <lida@edison.tech>
2019-08-08 09:17:29 +0800

    init project.

---
+++ edo/__init__.py
@@ -0,0 +1,4 @@
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.basicConfig(level = logging.DEBUG,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
