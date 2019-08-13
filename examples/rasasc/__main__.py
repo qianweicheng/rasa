7873472ea4260cf16f1d85845b0ff30c087ca56e
yuanlida <lida@edison.tech>
2019-08-13 14:00:22 +0800

    rasa edison

--- examples/rasasc/__main__.py
+++ examples/rasasc/__main__.py
@@ -4,7 +4,7 @@ import rasa
 
 
 if __name__ == "__main__":
-    os.chdir('/Users/lidayuan/Documents/edison/nlu/rasa/examples/rasasc')
+    # os.chdir('/Users/lidayuan/Documents/edison/nlu/rasa/examples/rasasc')
     rasa.train(domain='domain.yml', config='config.yml', training_files='./data')
     # rasa.run(model="models", endpoints="endpoints.yml")
 
