63e5adf720996780b38caed907310ad4497f03bb
yuanlida <lida@edison.tech>
2019-08-16 17:39:23 +0800

    new data.

--- examples/EdisonCustomService/run.py
+++ examples/EdisonCustomService/run.py
@@ -6,7 +6,7 @@ from typing import Text
 import os
 import rasa.utils.io
 import rasa.train
-from examples.rasasc.policy import EdisonPolicy
+from examples.EdisonCustomService.policy import EdisonPolicy
 from rasa.core.agent import Agent
 from rasa.core.policies.memoization import MemoizationPolicy
 from rasa.core.policies.mapping_policy import MappingPolicy
