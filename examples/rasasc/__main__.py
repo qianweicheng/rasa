430fe8d49e1b15cf50b1025c1b308690aef84f43
weicheng <weicheng.andy@gmail.com>
2019-10-22 23:18:34 +0800

    update note

--- examples/rasasc/__main__.py
+++ examples/rasasc/__main__.py
@@ -2,9 +2,7 @@ import os
 
 import rasa
 
-
 if __name__ == "__main__":
-    os.chdir(os.getcwd())
-    # os.chdir(os.getcwd()+"/examples/rasasc")
+    os.chdir(os.path.dirname(__file__))
     # rasa.train(domain='domain.yml', config='config.yml', training_files='./data')
     rasa.run(model="models", endpoints="endpoints.yml")
