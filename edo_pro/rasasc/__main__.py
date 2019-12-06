3fe476f0e0d6bde585d289dc901d56c06d3f4ad0
weicheng <weicheng.andy@gmail.com>
2019-12-06 10:38:18 +0800

    changes for deploy

--- edo_pro/rasasc/__main__.py
+++ edo_pro/rasasc/__main__.py
@@ -1,9 +1,37 @@
+import getopt
 import os
+import sys
 
-import rasa
+
+def help_me():
+    msg = '''
+Usage :
+    -h Show this message
+    -r run
+    -t train
+    '''
+    print(msg)
+
+
+def train():
+    import rasa
+    rasa.train(domain='domain.yml', config='config.yml', training_files='./data')
+
+
+def run():
+    import rasa
+    rasa.run(model="models", endpoints="endpoints.yml")
 
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
-    rasa.train(domain='domain.yml', config='config.yml', training_files='./data')
-    # rasa.run(model="models", endpoints="endpoints.yml")
+    opts, args = getopt.getopt(sys.argv[1:], "hrt")
+    for op, value in opts:
+        if op in ("-h",):
+            help_me()
+        elif op in ("-r",):
+            train()
+        elif op in ("-t",):
+            train()
+        else:
+            help_me()
