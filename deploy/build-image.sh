9d845bf88a9a870029c2451f5e598b6b460048bb
weicheng <weicheng.andy@gmail.com>
2020-01-31 17:19:37 +0800

    update Jenkinsfile

--- deploy/build-image.sh
+++ deploy/build-image.sh
@@ -3,5 +3,5 @@ set -e
 ROOT_DIR=$(cd "$(dirname "$0")/..";pwd)
 cd $ROOT_DIR
 
-docker build -t hub.edisonpark.net/edisonchat/rasa:tools -f ./deploy/Dockerfile .
+docker build -t hub.edisonpark.net/edisonchat/rasa:tools -f ./deploy/Dockerfile_tools .
 docker push hub.edisonpark.net/edisonchat/rasa:tools
